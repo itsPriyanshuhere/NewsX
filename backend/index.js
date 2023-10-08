344b1eb2d8942c0d4d63f590e101be2b4bbda585
Nimish-Dureja-10 <durejanimish14@gmail.com>
2023-10-08 12:06:21 +0530

    Api Call on / route and axios package installed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+const apiUrl = 'http://api.mediastack.com/v1/news';
+const apiKey = 'b679e769ab2a18682a48b7b99cdc7f59';
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        access_key: apiKey,
+      },
+    });
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 app.listen(3000,()=>{
